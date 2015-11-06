2b3784e93474ae1c6cb847457fad87b0b6357f27
rockwyc992 <rockwyc992@gmail.com>
2015-11-06 11:04:26 +0800

    Alarm template

---
+++ simple_clock/js/Alarm/Model.js
@@ -0,0 +1,11 @@
+(function(window) {
+    'use strict';
+
+    function Model() {
+    }
+
+    // Export to window
+    window.Alarm = window.Alarm || {};
+    window.Alarm.Model = Model;
+})(window);
+
