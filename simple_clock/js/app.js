a01201c655d86e8308b70476e023e41e033a09b6
rockwyc992 <rockwyc992@gmail.com>
2015-11-06 08:46:35 +0800

    Load MVC

--- simple_clock/js/app.js
+++ simple_clock/js/app.js
@@ -1,6 +1,25 @@
-window.addEventListener("load", function() {
-    console.log("Hello, this is a simple clock.");
+window.Load_MVC = function(name) {
+    $.when(
+        $.getScript('js/' + name + '/Model.js'),
+        $.getScript('js/' + name + '/View.js'),
+        $.getScript('js/' + name + '/Controller.js'),
+        $.Deferred(function( deferred ){
+            $( deferred.resolve );
+        })
+    ).done(function() {
+            var model;
+        var view;
 
+        model = new Clock.Model();
+        view = new Clock.View();
+        var clock = new Clock.Controller(model, view);
+        clock.start();
+    }).fail(function() {
+        console.log(name + 'MVC loading fail. ');
+    });
+}
+
+window.Tab_start = function() {
     $('ul.tabs li').each(function(i) {
         $(this).click(function() {
             $(this).parent().children().removeClass('enable').eq(i).addClass('enable');
@@ -8,18 +27,20 @@ window.addEventListener("load", function() {
         });
     });
 
-    (function() {
-        var model;
-        var view;
-
-        model = new this.Clock.Model();
-        view = new this.Clock.View();
-        var clock = new this.Clock.Controller(model, view);
-        clock.start();
-    })();
-
+    // set default tab
     $('ul.tabs li').children().removeClass('enable').eq(1).addClass('enable');
     $('section.main').children('div').hide().eq(1).show();
+}
+
+window.addEventListener("load", function() {
+    console.log("Hello, this is a simple clock.");
+
+    Load_MVC('Clock');
+    Load_MVC('Stopwatch');
+    // Load_MVC('Timer');
+    // Load_MVC('Alarm');
+
+    Tab_start();
 });
 
 
