a108e4125726a7c6aaf4d1e5062cfd637533f840
張勝韋 <g84569@yahoo.com.tw>
2015-11-10 23:37:35 +0800

    feature 1 complete

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,3 +1,10 @@
-window.addEventListener("load", function() {
-  console.log("Hello World!");
+window.addEventListener("load", function () {
+    document.querySelector("#main").style.height = screen.height + "px";
+    showTime();
 });
+
+function showTime() {
+    var now = new Date();
+    document.querySelector("#time").innerHTML =now.toLocaleDateString("zh-TW")+" "+now.toDateString().match("\\D{3}")+" "+now.toTimeString();
+    setTimeout(showTime, 1000);
+}
