4fcb5aa5e3ca738809235f9e4411e8791b4884bf
catLee <leemiyinghao@gmail.com>
2015-11-11 22:22:45 +0800

    Test tab switching

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,3 +1,16 @@
+function updateDate(timeLabel){
+    date = new Date();
+    timeLabel.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
 window.addEventListener("load", function() {
-  console.log("Hello World!");
+    timeLabel = document.querySelector("#timeLabel");
+    var updateTimeProccess = window.setInterval(updateDate, 1000, timeLabel);
 });
+window.addEventListener("click", function(event){
+    if(event.target.classList.contains('tab')){
+	var id = event.target.dataset.tabId;
+	console.log(id);
+	document.querySelector(".active").classList.remove("active");
+	document.querySelector("#"+id+"Page").classList.add("active");
+    }
+})
