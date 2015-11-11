c2816ef341d1cb70e8c9cb15daee26b6709724de
JasonChiu <jason1122g@gmail.com>
2015-11-11 21:46:00 +0800

    add stopwatch

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,4 +1,11 @@
 window.addEventListener("load", function() {
   var clockManager = new ClockManager();
   clockManager.start();
+  var stopWatchManager = new StopWatchManager( {
+    time:  'stopwatch-time',
+    start: 'stopwatch-start',
+    pause: 'stopwatch-pause',
+    reset: 'stopwatch-reset'
+  } );
+  stopWatchManager.init();
 });
