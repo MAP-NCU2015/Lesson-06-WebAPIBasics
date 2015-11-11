6b4b7b4266aa0aea13a19d85febb6b955a171900
張勝韋 <g84569@yahoo.com.tw>
2015-11-11 19:53:26 +0800

    feature 3 complete

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -6,13 +6,18 @@ var elapsed;
 var timoutid;
 var spbutton;
 var rebutton;
+var opbutton;
+var minute;
+var second;
 
+//f1 function
 function showTime() {
     var now = new Date();
     timetext.innerHTML = now.toLocaleDateString("zh-TW") + " " + now.toDateString().match("\\D{3}") + " " + now.toTimeString();
     setTimeout(showTime, 1000);
 }
 
+//f2 function
 function onStartPause() {
     if (spbutton.innerHTML == "開始") {
         spbutton.innerHTML = "暫停";
@@ -40,14 +45,47 @@ function updateWatch() {
     timoutid = setTimeout(updateWatch, 10);
 }
 
+//f3 function
+function onOpen() {
+    var check = /^\d+$/;
+    if (check.test(minute.value) && minute.value < 60 && check.test(second.value) && second.value < 60) {
+        var sche = new Date();
+        var data = {};
+        sche.setTime(sche.getTime() + minute.value * 60 * 1000 + second.value * 1000);
+        var request = navigator.mozAlarms.add(sche, "ignoreTimezone", data);
+        request.onsuccess = function () {
+            console.log("success");
+            alert("鬧鐘將在" + minute.value + "分" + second.value + "秒後執行");
+        };
+        request.onerror = function () {
+            console.log(this.error.name);
+        };
+        navigator.mozSetMessageHandler("alarm", function (mozAlarm) {
+            var options = {
+                icon: "/icons/icon16x16.png",
+                body: "幸運豬肚子餓了"
+            }
+            var n = new Notification("該起床囉", options);
+            setTimeout(n.close.bind(n), 5000);
+        });
+    }
+    else
+        alert("時間錯誤");
+}
+
+//init
 window.addEventListener("load", function () {
     document.querySelector("#main").style.height = screen.height + "px";
     timetext = document.querySelector("#time");
     watchtimetext = document.querySelector("#watchtime");
+    minute = document.querySelector("#minute");
+    second = document.querySelector("#second");
     spbutton = document.querySelector("#startpause");
     spbutton.addEventListener("click", onStartPause);
     rebutton = document.querySelector("#reset");
     rebutton.addEventListener("click", onReset);
+    opbutton = document.querySelector("#open");
+    opbutton.addEventListener("click", onOpen);
     watchtime = new Date(0);
     elapsed = new Date(0);
     showTime();
