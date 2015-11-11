e5b9ff166ca10cbab7919aecab8f1fa5c290d52e
CrashedBboy <crashedbboy@gmail.com>
2015-11-11 11:31:21 +0800

    Stop watch

--- simple_clock/scripts/app.js
+++ simple_clock/scripts/app.js
@@ -1,8 +1,13 @@
-var clock;
+var clock, stopwatch;
 $(document).ready(function() {
 	clock = new Clock();
 	clock.setWrapper("clock_timezone", "clock_week", 
 										"clock_year", "clock_month", "clock_date", 
 										"clock_hour", "clock_minute", "clock_second");
 	clock.start();
+
+	stopwatch = new Stopwatch();
+	stopwatch.setWrapper("stop_minute", "stop_second", "stop_msecond");
+	stopwatch.setTrigger("stop_start", "stop_pause",
+												"stop_resume", "stop_reset");
 });
--- simple_clock/scripts/stopwatch.js
+++ simple_clock/scripts/stopwatch.js
@@ -15,20 +15,40 @@
 			this.milliWrapper = milli;
 		},
 
+		setTrigger(start, pause, resume, reset) {
+			this.startTrigger = start;
+			this.pauseTrigger = pause;
+			this.resumeTrigger = resume;
+			this.resetTrigger = reset;
+			$("#" + this.startTrigger).click(this.start.bind(this));
+			$("#" + this.pauseTrigger).click(this.pause.bind(this)).hide();
+			$("#" + this.resumeTrigger).click(this.resume.bind(this)).hide();
+			$("#" + this.resetTrigger).click(this.reset.bind(this));
+		},
+
 		start() {
 			this.run = true;
-			setInterval(this.count.bind(this), 10);
+			setInterval(this.calculate.bind(this), 10);
+			$("#" + this.startTrigger).hide();
+			$("#" + this.pauseTrigger).show();
 		},
 
 		pause() {
 			this.run = false;
+			console.log("pause");
+			$("#" + this.pauseTrigger).hide();
+			$("#" + this.resumeTrigger).show();
 		},
 
 		resume() {
 			this.run = true;
+			console.log("resume");
+			$("#" + this.resumeTrigger).hide();
+			$("#" + this.pauseTrigger).show();
 		},
 
 		reset() {
+			console.log("reset");
 			this.run = 0;
 			this.count = 0;
 			this.preMinute = this.minute;
@@ -38,6 +58,9 @@
 			this.second = 0;
 			this.milli = 0;
 			this.update();
+			$("#" + this.startTrigger).show();
+			$("#" + this.pauseTrigger).hide();
+			$("#" + this.resumeTrigger).hide();
 		},
 
 		update() {
@@ -49,17 +72,22 @@
 			}
 			$("#" + this.milliWrapper).text(this.format(this.milli));
 		},
-		count() {
+
+		calculate() {
 			if (this.run) {
 				this.preMinute = this.minute;
 				this.preSecond = this.second;
 				this.preMilli = this.milli;
 				this.count += 10;
 				this.milli = (this.count % 1000) / 10;
-				this.second = this.count / 1000;
-				this.minute = this.second / 60;
+				this.second = Math.floor(this.count / 1000) % 60;
+				this.minute = Math.floor(this.count / 60000);
 				this.update();
 			}
+		},
+
+		format(num) {
+			return num>9 ? num : "0"+num;
 		}
 	}
 	exports.Stopwatch = Stopwatch;
