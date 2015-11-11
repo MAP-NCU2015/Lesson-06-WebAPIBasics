9e64b75e3ea03f42a70dac863387866703215bb6
CrashedBboy <crashedbboy@gmail.com>
2015-11-11 15:43:53 +0800

    Alarm

--- simple_clock/scripts/app.js
+++ simple_clock/scripts/app.js
@@ -1,4 +1,4 @@
-var clock, stopwatch;
+var clock, stopwatch, alarm;
 $(document).ready(function() {
 	clock = new Clock();
 	clock.setWrapper("clock_timezone", "clock_week", 
@@ -10,4 +10,8 @@ $(document).ready(function() {
 	stopwatch.setWrapper("stop_minute", "stop_second", "stop_msecond");
 	stopwatch.setTrigger("stop_start", "stop_pause",
 												"stop_resume", "stop_reset");
+
+	alarm = new Alarm();
+	alarm.setTrigger("alarm_start");
+	alarm.setField("alarm_minute", "alarm_second");
 });
