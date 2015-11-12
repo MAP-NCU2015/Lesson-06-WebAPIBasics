d7fe1652df8eb3e5c4055b29b913f4f40f392948
timmycheng1221 <cksh11336@gmail.com>
2015-11-12 23:07:52 +0800

    add an alarm(unfinished)

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,19 +1,4 @@
-window.addEventListener("load", function() {
-  init();
-});
-
-init = function() {
-  //main clock
-  clockcontent = document.getElementById("clock");
-  setclock();
-  setInterval(setclock, 10);
-
-  //timer
-  t = 0;
-  timercontent = document.getElementById("timer");
-}
-
-setclock = function() {
+function setclock() {
   var now = new Date(Date.now());
   var year = now.getFullYear();
   var month = 1 + now.getMonth();
@@ -51,14 +36,46 @@ setclock = function() {
   clockcontent.innerHTML = year + "/" + month + "/" + date + " " + day + " " + hour.slice(-2) + ":" + minute.slice(-2) + ":" + second.slice(-2) + "<br> 時區：UTC/GMT " + (timezone>0?"-":"+") + timezone / -60;
 }
 
-count = function() {
+function count() {
   t++; 
   timercontent.innerHTML = ("0" + parseInt(t / 6000)).slice(-2) + ":" + ("0" + parseInt(t / 100 % 60)).slice(-2) + "." + ("0" + parseInt(t % 100)).slice(-2);
 }
 
-reset = function() {
+function reset() {
   t = 0;
   clearInterval(c);
   timercontent.innerHTML = "00:00.00";
 }
+
+//main clock
+var clockcontent = document.getElementById("clock");
+setclock();
+setInterval(setclock, 10);
+
+//timer
+t = 0;
+var timercontent = document.getElementById("timer");
+
+//alarm
+var alarmbutton = document.getElementById("addalarm");
+alarmbutton.addEventListener('click' , function(event) {
+  var alarm_min = document.getElementById("alarm_min");
+  var alarm_sec = document.getElementById("alarm_sec");
+  var alarmtime = new Date();
+  alarmtime.setMinutes(alarmtime.getMinutes() + alarm_min.value);
+  alarmtime.setSeconds(alarmtime.getSeconds() + alarm_sec.value);
+  var data = {
+    foo: "bar"
+  }
+  var request = navigator.mozAlarms.add(alarmtime, "ignoreTimezone", data);
+  request.onsuccess = function () {
+    console.log("The alarm has been scheduled");
+  };
+  request.onerror = function () { 
+    console.log("An error occurred: " + this.error.name);
+  };
+  navigator.mozSetMessageHandler("alarm", function (request) { 
+    new Notification("Alarm!!");
+  });
+});
 
