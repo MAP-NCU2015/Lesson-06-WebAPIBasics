fe46578b895f001f33190b97a2501999d60e8523
BDSword <thetruebloodsword@gmail.com>
2015-11-12 10:09:50 +0800

    Basic clock complete.

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,3 +1,28 @@
 window.addEventListener("load", function() {
-  console.log("Hello World!");
+  var intervalID = window.setInterval(updateTime, 1000);
 });
+
+function updateTime() {
+  var content = document.querySelector('content');
+  content.innerHTML = getNowTimeString();
+}
+
+function getNowTimeString() {
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = now.getMonth();
+  var date = now.getDate();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+
+  return year + '/' + pad(month, 2) + '/' + pad(date, 2) + ' ' + pad(hour, 2) + ':' + pad(minute, 2) + ':' + pad(second, 2);
+}
+
+function pad(number, length) {
+  var str = '' + number;
+  while (str.length < length) {
+    str = '0' + str;
+  }
+  return str;
+}
