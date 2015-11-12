c56c0fc21da1a81a91275e3765891640ecb87d13
catLee <leemiyinghao@gmail.com>
2015-11-13 06:27:51 +0800

    timer not complete

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -24,6 +24,40 @@ function updateFontSize(){
     dateLabel.style.fontSize = (window.screen.width / 8) + "px";
     weekLabel.style.fontSize = (window.screen.width / 30) + "px";
     timezoneLabel.style.fontSize = (window.screen.width / 30) + "px";
+};
+function initTimerSelector(minuteSelector, secondSelector){
+    for(var i = 0; i < 60; i++){
+	minuteSelector.innerHTML += "<option>"+i+"</option>\n";
+	secondSelector.innerHTML += "<option>"+i+"</option>\n";
+    };
+};
+function addTimer(minuteSelector, secondSelector){
+    console.log("Add timer: "+minuteSelector.value+":"+secondSelector.value);
+    var date = new Date();
+    console.log(date.getTime());
+    date.setTime(date.getTime() + (1000*480*60) + (60000*(minuteSelector.value)) + (1000*(secondSelector.value)));
+    console.log(date.getTime());
+    var data = {
+	title: "hi"
+    }
+    var alarm = window.navigator.mozAlarms.add(data, "honorTimezone", date);
+    alarm.onerror = function(){
+	console.log("error");
+    }
+    var r = navigator.mozAlarms.getAll();
+
+    r.onsuccess = function () {
+	this.result.forEach(function (alarm) {
+	    console.log('Id: ' + alarm.id);
+	    console.log('date: ' + alarm.date);
+	    console.log('respectTimezone: ' + alarm.respectTimezone);
+	    console.log('data: ' + JSON.stringify(alarm.data));
+	});
+    };
+
+    r.onerror = function () {
+	console.log("An error occurred: " + this.error.name);
+    };
 }
 window.addEventListener("load", function() {
     clockLabel = document.querySelector("#clockLabel");
@@ -33,13 +67,24 @@ window.addEventListener("load", function() {
     weekLabel = document.querySelector("#weekLabel");
     timezoneLabel = document.querySelector("#timezoneLabel");
     var updateTimeProccess = window.setInterval(updateDate, 1000, clockLabel, yearLabel, monthLabel, dateLabel, weekLabel, timezoneLabel);
+    minuteSelector = document.querySelector("#minuteSelector");
+    seocndSelector = document.querySelector("#secondSelector");
+    initTimerSelector(minuteSelector, secondSelector);
 });
 window.addEventListener("click", function(event){
     if(event.target.classList.contains('tab')){
 	var id = event.target.dataset.tabId;
 	document.querySelector(".active").classList.remove("active");
 	document.querySelector("#"+id+"Page").classList.add("active");
+    }else if(event.target.id == "addTimerBtn"){
+	minuteSelector = document.querySelector("#minuteSelector");
+	seocndSelector = document.querySelector("#secondSelector");
+	addTimer(minuteSelector, secondSelector);
     }
 })
 window.onresize = updateFontSize();
 window.screen.orientation.onchange = function(){updateFontSize()};
+navigator.mozSetMessageHandler("alarm", function (mozAlarm) {
+    alert("alarm fired");
+    console.log('alarm');
+});
