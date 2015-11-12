a88a8dbb775ba3123da4b64c9ec5e85593c336a2
BDSword <thetruebloodsword@gmail.com>
2015-11-12 16:37:15 +0800

    Add week number and time zone support.

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -15,8 +15,21 @@ function getNowTimeString() {
   var hour = now.getHours();
   var minute = now.getMinutes();
   var second = now.getSeconds();
+  var week = now.getWeekNumber();
+  var timezone = now.getTimezoneOffset();
+  var timezoneString = 'UTC';
+  if (timezone > 0) {
+    timezoneString += '-';
+  } else {
+    timezoneString += '+';
+  }
 
-  return year + '/' + pad(month, 2) + '/' + pad(date, 2) + ' ' + pad(hour, 2) + ':' + pad(minute, 2) + ':' + pad(second, 2);
+  timezoneString = timezoneString + Math.abs(timezone) / 60 + 'hour ';
+  if (Math.abs(timezone) % 60 != 0) {
+    timezoneString = timezoneString + Math.abs(timezone) % 60 + 'min';
+  }
+
+  return year + '/' + pad(month, 2) + '/' + pad(date, 2) + ' ' + pad(hour, 2) + ':' + pad(minute, 2) + ':' + pad(second, 2) + '<br> Week: ' + week + ', Timezone: ' + timezoneString;
 }
 
 function pad(number, length) {
@@ -26,3 +39,10 @@ function pad(number, length) {
   }
   return str;
 }
+
+Date.prototype.getWeekNumber = function(){
+  var d = new Date(+this);
+  d.setHours(0, 0, 0);
+  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+  return Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7);
+};
