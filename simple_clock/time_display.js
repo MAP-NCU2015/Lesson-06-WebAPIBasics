43b9b8146e08b2b539f937265a37ef813d68a419
RichardYang <ylin0603@gmail.com>
2015-11-13 12:58:45 +0800

    fix display_time

--- simple_clock/time_display.js
+++ simple_clock/time_display.js
@@ -13,12 +13,35 @@
         showTime(){
             var today = new Date();
             var h1 = document.querySelector('#h1');
-            //h.textContent='';
-            //h.textContent=today.getFullYear()+" "+today.getMonth()+
-            //" "+today.getDate()+" "+today.getHour()+" "+today.getMinute()+
-            //" "+today.getSecond();
-            var textContent=today;
-            h1.innerHTML = "";
+            var day;
+            switch(today.getDay()){
+                case 1:
+                    day="Monday";
+                    break;
+                case 2:
+                    day="Tuesday";
+                    break;
+                case 3:
+                    day="Wednesday";
+                    break;
+                case 4:
+                    day="Thursday"
+                    break;
+                case 5:
+                    day="Friday";
+                    break;
+                case 6:
+                    day="Saturday";
+                    break;
+                case 0:
+                    day="Sunday";
+                    break;
+                    
+            }
+            var textContent=today.getFullYear()+"/"+today.getMonth()+
+            "/"+today.getDate()+"<br/>"+today.getHours()+":"+today.getMinutes()+
+            ":"+today.getSeconds()+"<br/>"+""+day+"<br/>"+"UTC "+today.getTimezoneOffset()/-60;
+            
             h1.innerHTML = textContent;
             
         }    
