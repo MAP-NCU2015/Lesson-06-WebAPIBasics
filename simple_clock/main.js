7f94a70b09b8db378f464fe1654df0d3ce564d75
Davisanity <davidlin12tw@gmail.com>
2015-11-13 13:51:42 +0800

    Update main.js
    ADD Syntax

--- simple_clock/main.js
+++ simple_clock/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    var clock = new Clock();
+document.addEventListener('DOMContentLoaded', function(event) {     //DOMContentLoaded事件是當document被完整的讀取跟解析後就會被觸發
+    var clock = new Clock();                                        //不會等待 stylesheets, 圖片和subframes完成讀取  (load事件可以用來作為判斷頁面已經完整讀取的方法).
     clock.showtime();
     setInterval(clock.showtime, 500);
     var stopwatch = new Stopwatch();
