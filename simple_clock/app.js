ee65d6d2ce5d4700e7fe8fdef30e6203adf368b7
Chi-Zer Huang <onlyfly34@gmail.com>
2015-11-13 12:31:52 +0800

    Finish request 1: display date,time,week,zone

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,3 +1,11 @@
-window.addEventListener("load", function() {
-  console.log("Hello World!");
-});
+var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function settime() {
+    var today = new Date();
+    console.log(-today.getTimezoneOffset());
+    document.clock.date.value = today.toLocaleDateString();
+    document.clock.time.value = today.toLocaleTimeString();
+    document.clock.week.value = week[today.getDay()];
+    document.clock.zone.value = "UTC+"+(today.getTimezoneOffset()/-60);
+    TimerID = setTimeout("settime()", 1000);
+}
