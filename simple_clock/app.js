88f0f4a26a0f4d4bcadfe64348371ad4379db174
Chi-Zer Huang <onlyfly34@gmail.com>
2015-11-13 14:40:51 +0800

    Finish request 2: add stopwatch feature.

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,11 +1,51 @@
 var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var timestart, minute = 0, second = 0, ms = 1;
+var start_status = false;
 
+//display data,time,week,zone
 function settime() {
     var today = new Date();
-    console.log(-today.getTimezoneOffset());
     document.clock.date.value = today.toLocaleDateString();
     document.clock.time.value = today.toLocaleTimeString();
     document.clock.week.value = week[today.getDay()];
     document.clock.zone.value = "UTC+"+(today.getTimezoneOffset()/-60);
     TimerID = setTimeout("settime()", 1000);
+}
+
+//display stop watch
+function start() {
+    console.log(start_status);
+    if(start_status == false){
+        console.log("start_status");
+        start_status = true;
+        timestart = setInterval(function(){
+            if ((ms % 100) == 0) {
+                second += 1;
+                ms = 1;
+            }
+            if (second > 0 && (second % 60) == 0) {
+                minute += 1;
+                second = 0;
+            }
+            t = minute + ":" + second + "." + ms;
+            document.getElementById("stopwatch_display").value = t;
+            ms += 1;
+        }, 10);
+    }
+    else
+        //actually do nothing
+        start_status = true;
+}
+
+function pause() {
+    start_status = false;
+    clearInterval(timestart);
+}
+
+function stop() {
+    start_status = false;
+    clearInterval(timestart);
+    ms = 0;
+    minute = second = 0;
+    document.getElementById("stopwatch_display").value = minute + ":" + second + "." + ms;
 }
