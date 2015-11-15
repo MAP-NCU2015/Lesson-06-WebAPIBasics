1ddc8e3614f1be64e53024273124f62ac3e5cdc2
Chi-Zer Huang <onlyfly34@gmail.com>
2015-11-15 20:42:39 +0800

    Finish request 3: add alarm clock feature

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,10 +1,11 @@
 var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var timestart, minute = 0, second = 0, ms = 1;
 var start_status = false;
+var today;
 
 //display data,time,week,zone
 function settime() {
-    var today = new Date();
+    today = new Date();
     document.clock.date.value = today.toLocaleDateString();
     document.clock.time.value = today.toLocaleTimeString();
     document.clock.week.value = week[today.getDay()];
@@ -14,9 +15,7 @@ function settime() {
 
 //display stop watch
 function start() {
-    console.log(start_status);
     if(start_status == false){
-        console.log("start_status");
         start_status = true;
         timestart = setInterval(function(){
             if ((ms % 100) == 0) {
@@ -49,3 +48,64 @@ function stop() {
     minute = second = 0;
     document.getElementById("stopwatch_display").value = minute + ":" + second + "." + ms;
 }
+
+function setalarm() {
+    var hour = parseInt(today.getHours());
+    var min = parseInt(today.getMinutes());
+    var sec = parseInt(today.getSeconds());
+    var setmin = parseInt(document.getElementById("setmin").value);
+    var setsec = parseInt(document.getElementById("setsec").value);
+    if (setmin > 59 || setmin < 0 || isNaN(setmin) || setsec > 59  || 
+        setsec < 0 || isNaN(setsec)) {
+        alert("Please enter correct value");
+    }
+    else {
+        sec = sec + setsec;
+        if (sec + setsec > 59) {
+            min += 1;
+            sec = (sec + setsec) % 60;
+        }
+
+        min = (min + setmin);
+        if (min + setmin > 59) {
+            hour += 1;
+            min = (min + setmin) % 60;
+        }
+ 
+        if (hour === 25) {
+            hour = 1;
+        }
+        document.getElementById("alarmtime").innerHTML = "鐘響時間："+hour + ":" + min + ":" + sec;
+    }
+    check(hour, min, sec);
+}
+
+function check(h, m, s) {
+    if (today.getHours() == h && today.getMinutes() == m && today.getSeconds() == s) {
+        //alert("Time is up");
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+        }
+        else if (Notification.permission === "granted") {
+            var notification = new Notification("起床嘍");
+        }
+        else if(Notification.permission !== "denied") {
+            Notification.requestPermission(function (permission) {        
+                if (permission === "granted") {
+                    var notification = new Notification("time is up!!");  
+                }
+            });
+        }
+    }
+    checktime = setTimeout(function () { check(h, m, s) }, 1000);
+}
+
+window.addEventListener('load', function () {
+    Notification.requestPermission(function (status) {
+        // This allows to use Notification.permission with Chrome/Safari
+        Notification.permission = "granted";
+        if (Notification.permission != status) {
+            Notification.permission = "granted";
+        }
+    });
+});
