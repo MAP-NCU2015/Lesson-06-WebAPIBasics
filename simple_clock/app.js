2df04ef034ec31202c378ffd1f0d7b401e301091
KaiBro <w181496@gmail.com>
2015-11-19 22:33:26 +0800

    finish stopwatch

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
   console.log("Hello World!");
-  window.setInterval(update, 1000);
+  setInterval(update, 1000);
+  StopWatch();
 });
 
 function getTime() {
@@ -46,4 +47,47 @@ function update() {
     week.innerHTML = "今天星期" +  getWeek();
     var timezone = document.querySelector('#timezone');
     timezone.innerHTML = "時區: UTC" + getTimeZone();
+}
+
+function StopWatch() {
+   var flag = 0;
+   var stopwatch = document.getElementById('stopwatch');
+   var start = document.getElementById('start');
+   var pause = document.getElementById('pause');
+   var reset = document.getElementById('reset');
+   var timer = 0;
+    var count = 0;
+   
+   stopwatch.innerHTML = "00:00:00";
+   
+   start.addEventListener('click', function (event) {
+      if(flag == 0) {
+         flag = 1;
+         timer = setInterval(function() {
+            count += 1;
+            var min = fillZero(Math.floor(count / 6000));
+            var sec =  fillZero(Math.floor(count / 100 % 60));
+            var ms =  fillZero(count % 100);
+            stopwatch.innerHTML = min + ":" + sec + ":" + ms;
+         }, 10);
+      } else {
+         alert('G_G ler');
+      }
+   });
+   
+    pause.addEventListener('click', function(event) {
+         if(flag == 1) {
+            flag = 0;
+            window.clearInterval(timer);
+         } else {
+            alert('G_G ler');
+         }
+   });
+   
+   reset.addEventListener('click', function(event) {
+         flag = 0;
+         count = 0;
+         window.clearInterval(timer);
+         stopwatch.innerHTML = "00:00:00";
+   });
 }
