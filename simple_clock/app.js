b2e16d15cf9a751f50bb206fcaaa2c312cb34f33
Peter <peter654qq@gmail.com>
2015-12-03 23:07:14 +0800

    fix the bug of the alarm

--- simple_clock/app.js
+++ simple_clock/app.js
@@ -7,36 +7,36 @@ clock = function(){
 
 displayDate = function(){
 	var now  = new Date();
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var date = now.getDate();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    var day = now.getDay();
-    var timezone = now.getTimezoneOffset() / 60 * -1;
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1;
+  var date = now.getDate();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  var day = now.getDay();
+  var timezone = now.getTimezoneOffset() / 60 * -1;
 	switch(day) {
-        case 0:
-        day = '星期日';
-		break;
-        case 1:
-		day = '星期一';
-		break;
+    case 0:
+    	day = '星期日';
+			break;
+    case 1:
+			day = '星期一';
+			break;
 		case 2:
-		day = '星期二';
-		break;
+			day = '星期二';
+			break;
 		case 3:
-		day = '星期三';
-		break;
+			day = '星期三';
+			break;
 		case 4:
-		day = '星期四';
-		break;
+			day = '星期四';
+			break;
 		case 5:
-		day = '星期五';
-		break;
+			day = '星期五';
+			break;
 		case 6:
-		day = '星期六';
-		break;
+			day = '星期六';
+			break;
 	}
 	if(hour < 10) {
 		hour = '0' + hour;
@@ -59,14 +59,12 @@ StopWatch = function(){
 	isRunning = false;
 	stopWatch = document.getElementById('StopWatch');
 
-
-	if(millisec=="0"||millisec=="1"||millisec=="2"||millisec=="3"||millisec=="4"||millisec=="5"||millisec=="6"||millisec=="7"||millisec=="8"||millisec=="9")
-        millisec = "0" + millisec;
-    if(sec=="0"||sec=="1"||sec=="2"||sec=="3"||sec=="4"||sec=="5"||sec=="6"||sec=="7"||sec=="8"||sec=="9")
-        sec = "0" + sec;
-    if(min=="0"||min=="1"||min=="2"||min=="3"||min=="4"||min=="5"||min=="6"||min=="7"||min=="8"||min=="9")
-        min = "0" + min;
-
+	if(millisec < 10)
+      millisec = "0" + millisec;
+  if(sec <10)
+      sec = "0" + sec;
+  if(min <10)
+      min = "0" + min;
 	var displayString = min +":"+sec +"."+millisec ;
 	stopWatch.textContent = displayString;
 
@@ -87,23 +85,25 @@ StartClicked = function(){
 }
 
 counting = function(){
-	millisec++;
-	if(millisec == 100) {
-		sec++;
-		millisec = 0;
+	if (isRunning == true){
+		millisec++;
+		if(millisec == 100) {
+			sec++;
+			millisec = 0;
     }
-	if(sec == 60) {
-		min++;
-		sec = 0;
+		if(sec == 60) {
+			min++;
+			sec = 0;
+		}
+		if(millisec=="0"||millisec=="1"||millisec=="2"||millisec=="3"||millisec=="4"||millisec=="5"||millisec=="6"||millisec=="7"||millisec=="8"||millisec=="9")
+	      millisec = "0" + millisec;
+	  if(sec=="0"||sec=="1"||sec=="2"||sec=="3"||sec=="4"||sec=="5"||sec=="6"||sec=="7"||sec=="8"||sec=="9")
+	      sec = "0" + sec;
+	  if(min=="0"||min=="1"||min=="2"||min=="3"||min=="4"||min=="5"||min=="6"||min=="7"||min=="8"||min=="9")
+	      min = "0" + min;
+		var displayString = min +":"+sec +"."+millisec ;
+		stopWatch.textContent = displayString;
 	}
-	if(millisec=="0"||millisec=="1"||millisec=="2"||millisec=="3"||millisec=="4"||millisec=="5"||millisec=="6"||millisec=="7"||millisec=="8"||millisec=="9")
-        millisec = "0" + millisec;
-    if(sec=="0"||sec=="1"||sec=="2"||sec=="3"||sec=="4"||sec=="5"||sec=="6"||sec=="7"||sec=="8"||sec=="9")
-        sec = "0" + sec;
-    if(min=="0"||min=="1"||min=="2"||min=="3"||min=="4"||min=="5"||min=="6"||min=="7"||min=="8"||min=="9")
-        min = "0" + min;
-	var displayString = min +":"+sec +"."+millisec ;
-	stopWatch.textContent = displayString;
 }
 
 PauseClicked = function(){
@@ -116,17 +116,15 @@ PauseClicked = function(){
 ResetClicked = function(){
 	min = 0;
 	sec = 0;
-    millisec = 0;
+  millisec = 0;
 	clearInterval(timer);
 	isRunning = false;
-
-	if(millisec=="0"||millisec=="1"||millisec=="2"||millisec=="3"||millisec=="4"||millisec=="5"||millisec=="6"||millisec=="7"||millisec=="8"||millisec=="9")
-        millisec = "0" + millisec;
-    if(sec=="0"||sec=="1"||sec=="2"||sec=="3"||sec=="4"||sec=="5"||sec=="6"||sec=="7"||sec=="8"||sec=="9")
-        sec = "0" + sec;
-    if(min=="0"||min=="1"||min=="2"||min=="3"||min=="4"||min=="5"||min=="6"||min=="7"||min=="8"||min=="9")
-        min = "0" + min;
-				
+	if(millisec < 10)
+      millisec = "0" + millisec;
+  if(sec <10)
+      sec = "0" + sec;
+  if(min <10)
+      min = "0" + min;
 	var displayString = min +":"+sec +"."+millisec ;
 	stopWatch.textContent = displayString;
 }
@@ -158,7 +156,7 @@ alarm = function(){
 	setAlarmButton.addEventListener("click",SetAlarmButton.bind(this));
 
 	navigator.mozSetMessageHandler("alarm", function (alarm) {
-		new Notification("ALARM!!");
+		new Notification("ALARM!");
 	});
 }
 
@@ -167,5 +165,12 @@ SetAlarmButton = function(){
 	var setTime = new Date();
 	setTime.setHours(hour.options[hour.selectedIndex].value);
 	setTime.setMinutes(minute.options[minute.selectedIndex].value);
+	setTime.setSeconds(0);
+	setTime.setMilliseconds(0);
+
+  if (Now.getHours() > setTime.getHours() || (Now.getHours() == setTime.getHours() && Now.getMinutes() >= setTime.getMinutes())){
+		setTime.setDate(Now.getDate()+1);
+	}
+
 	var request = navigator.mozAlarms.add(setTime, "ignoreTimezone");
 }
